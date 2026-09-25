b474f770309fe39a45d0f6485e7aa1310383a815
Dinislam2009 <123666107+Dinislam2009@users.noreply.github.com>
2026-09-25 16:16:43 +0500

    chore: remove unused image occlusion import

--- src/components/image-occlusion.tsx
+++ src/components/image-occlusion.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export type OcclusionRect = { x:number; y:number; w:number; h:number };
 
